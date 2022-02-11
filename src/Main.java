a1634cae194c153ddfa6cf8150e0d7b55d0eaced
Mckelvin Ofosu-Frimpong <mickeyfrimpong@Mckelvins-MBP.home>
2022-02-11 16:19:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+	int numOne = 6;
+    int numTwo = 2;
+
+    if (numOne < numTwo)
+    {
+        System.out.println(numOne + " is less than " + numTwo);
+    }
+    else if (numOne == numTwo )
+    {
+        System.out.println(numOne + " and " + numTwo + " are equal ");
+    }
+    else
+    {
+        System.out.println(numTwo + " is less than " + numOne);
+    }
+
+    }
+}
